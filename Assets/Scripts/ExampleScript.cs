f150ec33f04333b0d89fc96aac72bac13522d011
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2016-05-03 00:42:29 +0900

    プロジェクト追加

---
+++ Assets/Scripts/ExampleScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using System.Collections.Generic;
+
+public class ExampleScript : MonoBehaviour
+{
+	[SerializeField]
+	private SSGuidGenerator generator;
+
+	[SerializeField]
+	private int generateCount;
+
+	void Start()
+	{
+		for(int i=0; i<this.generateCount; i++)
+		{
+			Debug.Log(this.generator.New);
+		}
+	}
+}
---
+++ Assets/Scripts/SSGuidGenerator.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using System.Collections.Generic;
+using System.Text;
+
+/// <summary>
+/// シルベスター・スタローンGUIDジェネレーター.
+/// </summary>
+[CreateAssetMenu()]
+public class SSGuidGenerator : ScriptableObject
+{
+	[SerializeField]
+	private string header;
+
+	[SerializeField]
+	private string footer;
+
+	[SerializeField]
+	private List<string> identities;
+
+	[SerializeField]
+	private int length = 256;
+
+	public string New
+	{
+		get
+		{
+			var builder = new StringBuilder();
+			builder.Append(this.header);
+			for(int i=0; i<this.length; i++)
+			{
+				builder.Append(this.identities[UnityEngine.Random.Range(0, this.identities.Count)]);
+			}
+			builder.Append(this.footer);
+
+			return builder.ToString();
+		}
+	}
+}
